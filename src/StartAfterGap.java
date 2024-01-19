7613ec5988feb7e9b970d374cfee32688afc34d1
Muhammad Jafar <muhammadjafarrahimjonzoda@gmail.com>
2024-01-19 22:01:53 +0300

    Let's see

---
+++ src/StartAfterGap.java
@@ -0,0 +1,10 @@
+
+public class StartAfterGap {
+
+	public static void main(String[] args) {
+		System.out.println("Bismillah");
+		System.out.println("I will try to upload the projects, which i've done during this unprecedented semeter.");
+
+	}
+
+}
